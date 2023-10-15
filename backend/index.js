72a5c71db6db4f9c0826717f7d77f880436ee9c2
Pavitra112 <102229923+Pavitra112@users.noreply.github.com>
2023-10-15 15:50:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 
 //for testing
 app.get('/', (req, res) => {
-   res.send('API is working'+process.env.MONGO_URI)
+   res.send('API is working')
 })
 
 //database connection
