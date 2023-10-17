c94a4357520f94074a143bcc12ce95523e5dba6a
Pavitra112 <102229923+Pavitra112@users.noreply.github.com>
2023-10-17 20:08:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -71,7 +71,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://toursmern.vercel.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Authorization');
   res.header('X-Content-Type-Options', 'nosniff');
   next();
 });
