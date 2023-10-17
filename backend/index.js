46f29ea3ad4dc7649cd9bbf338a5042e17486915
Pavitra112 <102229923+Pavitra112@users.noreply.github.com>
2023-10-17 08:54:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('X-Content-Type-Options', 'nosniff');
   next();
 });
 //for testing
